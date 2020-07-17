61972ee63e5387bc628b4a0f85932c13807d182c
Caleb <bargercj20@hotmail.com>
2020-07-17 11:03:11 -0700

    ok hamered out rb

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,32 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
 
     def append(self, item):
-        pass
+        # check len of array
+
+        self.data.append(item)
+        if len(self.data) == self.capacity:
+            self.cur = 0
+            self.append = self._full_append
+
+    def _full_append(self, item):
+        self.data[self.cur]=item
+        self.cur=(self.cur + 1) % self.capacity
+        
 
     def get(self):
-        pass
+        return self.data
+
+
+# rb = RingBuffer(5)
+# rb.append('a')
+# rb.append('b')
+# rb.append('c')
+# rb.append('d')
+# rb.append('e')
+# rb.append('f')
+# print(rb.get())
+
+
